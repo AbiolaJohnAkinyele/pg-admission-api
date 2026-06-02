fc2a6394c44ddfa48f6d827337982e72f2bd738b
Abiola Akinyele <biola.akinyele@gmail.com>
2026-06-02 22:34:15 +0100

    second Updated for adding candidate information

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -4,27 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Candidate;
+use Illuminate\Support\Facades\DB;
 
 
 class CandidateController extends Controller
 {
-    public function index()
-{
-    return response()->json(
-        Candidate::all()
-    );
-}
+    // public function index()
+    // {
+    // $candidates = Candidate::with('admission')->get();
+
+    // return response()->json($candidates);
+    // }
 
     public function show($id)
-    {
-        $candidate = Candidate::with('admission')->find($id);
+{
+    $candidate = DB::table('new')
+        ->join('zmain_app', 'new.id', '=', 'zmain_app.user_id')
+        ->leftJoin('fac_new', 'zmain_app.faculty', '=', 'fac_new.id')
+        ->leftJoin('dept_new', 'zmain_app.department', '=', 'dept_new.id')
+        ->select(
+            'new.id',
+            'new.numeration',
+            'new.nationality',
+            'new.Surname',
+            'new.Other_names',
+            'new.email',
+            'new.Telephone',
 
-        if (!$candidate) {
-            return response()->json([
-                'message' => 'Candidate not found'
-            ], 404);
-        }
+            'zmain_app.state_of_origin',
+            'zmain_app.local_govt_area',
+            'zmain_app.mode_of_study',
+            'zmain_app.field_of_interest',
 
-        return response()->json($candidate);
+            'fac_new.faculty as faculty_name',
+            'dept_new.department as department_name'
+        )
+        ->where('new.id', $id)
+        ->first();
+
+    if (!$candidate) {
+        return response()->json([
+            'message' => 'Candidate not found'
+        ], 404);
     }
+
+    return response()->json($candidate);
+}
 }
